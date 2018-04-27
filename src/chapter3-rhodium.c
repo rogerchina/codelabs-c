eb241ef12687428e622ace74fddee5e112c2baf6
roger.zhang <zhangyonghuifang@163.com>
2018-04-28 06:46:18 +0800

    add

--- src/chapter3-rhodium.c
+++ src/chapter3-rhodium.c
@@ -7,9 +7,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<inttypes.h>
-#include<limits.h>
+#include <string.h>
+#include <inttypes.h>
+#include <limits.h>
 
 #define DENSITY 62.4
 
